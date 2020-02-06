6c40e82dd560ea35303bef761e98dccc26862075
jeffchao98 <philadelphiafs@gmail.com>
2020-02-06 16:24:47 +0900

    MISC - Remote included - import Gson - Updated the version info inside the gradle script

---
+++ testframework/src/main/java/com/scaine/testframework/DataModel.java
@@ -0,0 +1,10 @@
+package com.scaine.testframework;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DataModel {
+    @SerializedName("name")
+    String strName = "";
+
+    String state = "";
+}
--- testframework/src/main/java/com/scaine/testframework/MainPart.java
+++ testframework/src/main/java/com/scaine/testframework/MainPart.java
@@ -1,7 +1,14 @@
 package com.scaine.testframework;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class MainPart {
     public String getName() {
-        return "Hello, this is MainPart";
+
+        String jsonData = "{\"name\":\"MainPart item\" , \"state\": \"unknown\"}";
+        DataModel dataModel = new Gson().fromJson(jsonData, new TypeToken<DataModel>() {
+        }.getType());
+        return dataModel.strName;
     }
 }
